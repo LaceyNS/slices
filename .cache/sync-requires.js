84acadc738ed62c8395dd09814401542f919180b
Lacey Savage <lacey.savage@gmail.com>
2020-11-03 17:38:07 -0500

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/laceysavage/sites/personalProjects/masterGatsby/slickSlices/gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/laceysavage/sites/personalProjects/masterGatsby/slickSlices/gatsby/src/pages/404.js"))),
+  "component---src-pages-beers-js": hot(preferDefault(require("/Users/laceysavage/sites/personalProjects/masterGatsby/slickSlices/gatsby/src/pages/beers.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/laceysavage/sites/personalProjects/masterGatsby/slickSlices/gatsby/src/pages/index.js"))),
+  "component---src-pages-orders-js": hot(preferDefault(require("/Users/laceysavage/sites/personalProjects/masterGatsby/slickSlices/gatsby/src/pages/orders.js"))),
+  "component---src-pages-pizza-js": hot(preferDefault(require("/Users/laceysavage/sites/personalProjects/masterGatsby/slickSlices/gatsby/src/pages/pizza.js"))),
+  "component---src-pages-slicemasters-js": hot(preferDefault(require("/Users/laceysavage/sites/personalProjects/masterGatsby/slickSlices/gatsby/src/pages/slicemasters.js")))
+}
+
