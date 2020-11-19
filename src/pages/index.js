a9097782b77b3966ed3ff263cc1b1cf92d31728f
Lacey Savage <lacey.savage@gmail.com>
2020-11-19 16:33:44 -0500

    add gatsby-browser and ssr, alter Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
-    <Layout>
-      <Nav />
+    <>
       <p>Hey! I'm the Home page</p>
-    </Layout>
+    </>
   );
 }
--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function PizzaPage() {
   return (
-    <div>
+    <>
       <p>Hey! I'm the Pizza page!</p>
-    </div>
+    </>
   );
 }
