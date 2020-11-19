6784ede7c34f216d42cb85aca90d6943edb1a8df
Lacey Savage <lacey.savage@gmail.com>
2020-11-19 15:21:16 -0500

    add example of button using navigate with a timeout. To be removed from project

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, navigate } from 'gatsby';
+
+function goToSlicemasters() {
+  // 1. Wait for 2 seconds
+  setTimeout(() => {
+    console.log('Go to Slicemasters!!!');
+    // 2. Change the page
+    navigate('/slicemasters');
+    // if we wanted to add it to the browser history, add { replace: true }
+  }, 2000);
+}
 
 export default function Nav() {
   return (
@@ -10,6 +20,12 @@ export default function Nav() {
         </li>
         <li>
           <Link to="/beers">Beers</Link>
+        </li>
+        <li>
+          <button type="button" onClick={goToSlicemasters}>
+            {' '}
+            Click me to see slicemasters after 2 seconds
+          </button>
         </li>
       </ul>
     </nav>
